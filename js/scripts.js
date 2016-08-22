8621c37c0de65e03308ff60f3d316f631ff451bb
Andres Garcia and Nicolas Hurtado <andres@>
2016-08-22 10:45:13 -0400

    create a form that collects information contact information from the user and stores it in a list, then when the object is clicked on the list, more information can be accessed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
-//business logic
-function Contact(first, last) {
+// Business Logic
+function Contact(first, last, address, city, state) {
   this.firstName = first;
   this.lastName = last;
-  this.addresses = [];
-}
-
-function Address(street, city, state) {
-  this.street = street;
+  this.address = address;
   this.city = city;
   this.state = state;
 }
@@ -15,67 +11,37 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
+Contact.prototype.fullAdress = function() {
+  return this.address + " " + this.city + ", " + this.state;
 }
+// user interface Logic
 
-function resetFields() {
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-}
-
-// user interface logic
-$(document).ready(function() {
-  
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
-  });
-
+$(document).ready(function () {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+   var inputtedFirstName = $("input#new-first-name").val();
+   var inputtedLastName = $("input#new-last-name").val();
+   var inputtedAddress = $("input#new-home-address").val();
+   var inputtedCity = $("input#new-city").val();
+   var inputtedState = $("input#new-state").val();
 
-    $(".new-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
-    });
+   var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress, inputtedCity, inputtedState);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+   $("ul#contacts").append("<li><span class = 'contact'>" + newContact.fullName() + "</span></li>");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
+   $(".contact").last().click(function () {
+     $("#show-contact").show();
+     $("#show-contact h2").text(newContact.firstName);
+     $(".first-name").text(newContact.firstName);
+     $(".last-name").text(newContact.lastName);
+     $(".address").text(newContact.fullAdress());
+   });
 
-    resetFields();
-
+   $("input#new-first-name").val(""); // this will clear out the form
+   $("input#new-last-name").val("");
+   $("input#new-home-address").val("");
+   $("input#new-city").val("");
+   $("input#new-state").val("");
   });
 });
